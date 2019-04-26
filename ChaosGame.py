06b1ed6a57ae857e3fc54d649a8028a6dddbc16a
J M <wohaste@gmail.com>
2019-04-26 14:30:22 -0600

    chaos game - started

--- ChaosGame.py
+++ ChaosGame.py
@@ -12,6 +12,7 @@
 #Suggestion is to never use the same Vertex twice in a row
 
 from PIL import Image, ImageDraw
+import random
 from Utilities import *
 
 #Runs the chaos game on the provided polygon and draw the result
@@ -21,9 +22,40 @@ class ChaosGame:
 		self.draw = ImageDraw.Draw(self.image)
 		
 		self.outline(listVertices)
-		self.generate(listVertices)
+		 #TODO: get random point in triangle
+		self.generate(listVertices, Point(300, 310), previousVertex=None)
 	#draw the outline of the polygon
 	def outline(self, listVertices):
-		pass
+		self.draw.line([point.toTuple() for point in listVertices], fill='gray', width=1)
+		self.draw.line([listVertices[0].toTuple(), listVertices[-1].toTuple()], fill='gray', width=1)
+	#generate and draw the next point
+	def generate(self, listVertices, pointReference, previousVertex):
+		for i in range(10000):
+			nextVertex = self.getRandomVertex(listVertices, previousVertex);
+			fullDistance = pointReference.distance(nextVertex)
+			nextPoint = Geometry.getPointBetweenPoints(pointReference, nextVertex, fullDistance / 2)
+			self.draw.point([nextPoint.toTuple()], fill='gray')
+			pointReference = nextPoint
+			previousVertex = nextVertex
+	@staticmethod
+	#returns a random vertex that is not the previous vertex
+	def getRandomVertex(listVertices, previousVertex):
+		if (len(listVertices) < 2):
+			raise Exception("Requires at least 2 vertices.")
+		while(True):
+			nextVertex = random.choice(listVertices)
+			if (previousVertex == None or previousVertex != nextVertex):
+				return nextVertex
 		
-x = Point(3,4)
+#################################
+
+#equilateral triangle centered in image
+image = Image.new('RGB', (800, 800), 'white')
+center = Point(400, 475)
+distance = 400
+points = [Point(center.x, center.y - distance)] #top of triangle
+points.append(Geometry.rotatePointAroundPoint(points[0], center, degrees = 120)) #bottom-left of triangle
+points.append(Geometry.rotatePointAroundPoint(points[0], center, degrees = -120)) #bottom-right of triangle
+chaosGame = ChaosGame(image, points);
+image.save('output/chaos_game_sierpinski_triangle.png')
+
